758aba0f5eea3b5a927f323f4b6a4676d6732b11
Matibois <100830614+Matibois@users.noreply.github.com>
2023-11-14 20:44:48 +0100

    multiclient / heartbeat / abort() error
    connexion se fait, se maintient pas et erreur abort(), ajout de code dans serveur et client

--- Server/Server.h
+++ Server/Server.h
@@ -1,1 +1,77 @@
 #pragma once
+
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include "SFML/Graphics.hpp"
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include "Game.h"
+#include <mutex>
+
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "1027"
+
+
+class Server {
+
+private:
+    Game* game = Game::Instance();
+    void shutdownClient(SOCKET clientSocket);
+    std::string generateSessionID() const;
+    void handleHeartbeats();
+    void handleClient(SOCKET clientSocket, const std::string& sessionID);
+    void initWSA();
+    void initSocket();
+    void listenClient();
+    void accepteClient();
+
+    WSADATA wsaData;
+    int iResult;
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    struct addrinfo* result = NULL;
+    struct addrinfo hints;
+
+    int iSendResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    std::vector<SOCKET> clients; // Liste des sockets des clients connectés
+    std::mutex clientsMutex; // Mutex pour protéger l'accès à la liste des clients
+
+
+    struct HeartbeatInfo {
+        SOCKET socket;
+        std::string sessionID;
+        std::chrono::steady_clock::time_point lastHeartbeatTime;
+    };
+
+    // Liste des clients avec informations de cœur
+    std::vector<HeartbeatInfo> heartbeatClients;
+    std::mutex heartbeatMutex; // Mutex pour protéger l'accès à la liste des clients
+
+    // Fréquence des cœurs en millisecondes
+    const int HEARTBEAT_INTERVAL = 200; // par exemple, toutes les 5 secondes
+
+
+public:
+    Server();
+    ~Server();
+    int start();
+    // Ajoute d'autres méthodes et membres au besoin.
+
+};
