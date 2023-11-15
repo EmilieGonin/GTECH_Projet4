a460edf4808c0c432d3b267787a252d226acfe55
Crimson41 <alexis.plp41@gmail.com>
2023-11-15 17:15:06 +0100

    refonte classe avec ServerWeb et ServerClient

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,10 +1,13 @@
 #include "Server.h"
 #include <thread>
 
-
-Server::Server() {}
-Server::~Server() {}
-
+Server::Server()
+{
+	initWSA();
+	initSocket();
+	listenClient();
+}
+Server::~Server() {};
 
 int __cdecl main(void)
 {
@@ -12,45 +15,10 @@ int __cdecl main(void)
 	server.start();
 }
 
-int Server::start()
-{
-	//game->init();
-	//while (game->isOpen()) game->update();
-
-	initWSA();
-	initSocket();
-
-	listenClient();
-	closesocket(ListenSocket);
-
-	// cleanup
-	closesocket(ClientSocket);
-	WSACleanup();
-
-	return 0;
-}
-
-
-
-void Server::initWSA()
-{
-	// Initialize Winsock
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		printf("WSAStartup failed with error: %d\n", iResult);
-	}
-
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	//hints.ai_flags = AI_PASSIVE;
-}
-
 void Server::initSocket()
 {
 	// Resolve the server address and port
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	iResult = getaddrinfo(NULL, mPort.c_str(), &hints, &result);
 	if (iResult != 0) {
 		printf("getaddrinfo failed with error: %d\n", iResult);
 		WSACleanup();
@@ -63,6 +31,47 @@ void Server::initSocket()
 		freeaddrinfo(result);
 		WSACleanup();
 	}
+}
+
+void Server::accepteClient()
+{
+	// Accept a client socket
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		printf("accept failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return;
+	}
+
+	printf("Client accepted.\n");
+
+	// Attribuer un identifiant de session au client
+	std::string sessionID = generateSessionID();
+	send(ClientSocket, sessionID.c_str(), sessionID.size(), 0);
+
+	std::lock_guard<std::mutex> lock(clientsMutex);
+	clients.push_back(ClientSocket);
+
+	handleClient(ClientSocket, sessionID);
+}
+
+void Server::shutdownClient(SOCKET clientSocket)
+{
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+	}
+
+	// Attendre quelques instants pour permettre la fermeture propre du socket
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+	clients.erase(std::remove_if(clients.begin(), clients.end(),
+		[clientSocket](SOCKET s) { return s == clientSocket; }), clients.end());
+
+	closesocket(clientSocket);
 }
 
 void Server::listenClient()
@@ -95,27 +104,43 @@ void Server::listenClient()
 	accepteClient();
 }
 
-void Server::accepteClient()
+int Server::start()
 {
-	// Accept a client socket
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	if (ClientSocket == INVALID_SOCKET) {
-		printf("accept failed with error: %d\n", WSAGetLastError());
-		closesocket(ListenSocket);
-		WSACleanup();
-		return;
+	//game->init();
+	//while (game->isOpen()) game->update();
+
+	closesocket(ListenSocket);
+
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
+}
+
+void Server::initWSA()
+{
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
 	}
 
-	printf("Client accepted.\n");
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	//hints.ai_flags = AI_PASSIVE;
+}
 
-	// Attribuer un identifiant de session au client
-	std::string sessionID = generateSessionID();
-	send(ClientSocket, sessionID.c_str(), sessionID.size(), 0);
+std::string Server::generateSessionID() const {
+	// Implémente une logique pour générer un identifiant de session unique
+	// Dans un contexte de production, tu pourrais utiliser une bibliothèque dédiée à la génération d'UUID ou d'autres méthodes de génération d'identifiants uniques.
+	// Pour cette démo, un simple timestamp pourrait suffire.
+	auto now = std::chrono::system_clock::now();
+	auto timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
 
-	std::lock_guard<std::mutex> lock(clientsMutex);
-	clients.push_back(ClientSocket);
-
-	handleClient(ClientSocket, sessionID);
+	return "SessionID_" + std::to_string(timestamp);
 }
 
 void Server::handleClient(SOCKET clientSocket, const std::string& sessionID) {
@@ -147,35 +172,5 @@ void Server::handleClient(SOCKET clientSocket, const std::string& sessionID) {
 			closesocket(ClientSocket);
 			WSACleanup();
 		}
-
 	} while (iResult > 0);
-}
-
-void Server::shutdownClient(SOCKET clientSocket)
-{
-	iResult = shutdown(ClientSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR) {
-		printf("shutdown failed with error: %d\n", WSAGetLastError());
-		closesocket(ClientSocket);
-		WSACleanup();
-	}
-
-	// Attendre quelques instants pour permettre la fermeture propre du socket
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-	clients.erase(std::remove_if(clients.begin(), clients.end(),
-		[clientSocket](SOCKET s) { return s == clientSocket; }), clients.end());
-
-	closesocket(clientSocket);
-
-}
-
-std::string Server::generateSessionID() const {
-	// Implémente une logique pour générer un identifiant de session unique
-	// Dans un contexte de production, tu pourrais utiliser une bibliothèque dédiée à la génération d'UUID ou d'autres méthodes de génération d'identifiants uniques.
-	// Pour cette démo, un simple timestamp pourrait suffire.
-	auto now = std::chrono::system_clock::now();
-	auto timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
-
-	return "SessionID_" + std::to_string(timestamp);
 }
