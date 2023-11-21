f027e279e378d5a8cbf9fd24f9f596e8d7e611eb
Justine-dt <72021396+Justine-dt@users.noreply.github.com>
2023-11-21 12:11:04 +0100

    add color text menu

--- Client/Window.h
+++ Client/Window.h
@@ -24,10 +24,13 @@ public:
 	void Finito();
 	void initTextFirstMenu();
 	void initTextSecondMenu();
+	void menuNameEnter();
+	void changeMenuColor();
 private:
 	sf::RenderWindow* mWindow;
 	std::vector<sf::Shape*> mShapes;
 	std::vector<sf::Text*> mTexts;
+	std::vector<sf::Text*> mTextMenu;
 	std::map<std::pair<int, int>, struct cell> mCells;
 	sf::Font mFont;
 	sf::Font mFontTitle;
@@ -40,4 +43,6 @@ private:
 	bool hasEnterName = true;
 	std::string mName;
 	sf::Text mEnterName;
+	sf::Event event;
+
 };
